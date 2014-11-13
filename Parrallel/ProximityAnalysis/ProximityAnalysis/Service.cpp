1149049bb9bf0025ec6abee62064fc1beae80974
Tyler Garrow <tylerg.ca@gmail.com>
2014-11-13 17:29:37 -0500

    implemented seperate IO striped file reading, TODO data collection threw message passing

--- Parrallel/ProximityAnalysis/ProximityAnalysis/Service.cpp
+++ Parrallel/ProximityAnalysis/ProximityAnalysis/Service.cpp
@@ -29,9 +29,19 @@ const string SERVICES_CODES_PATH = "data/service-codes.csv";
 	/*Constructors*/
 service::service(std::string code) : m_serivceTypeCode(code)
 { 
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	
+
 	//grab the locations of services
-	ifstream file;
-	file.open(SERVICES_DATA_PATH.c_str());
+	ifstream file(SERVICES_DATA_PATH.c_str());
+	if (!file.is_open() || file.fail())
+	{
+		cerr << "ERROR (process " << rank << "): Could not open file '"
+			<< SERVICES_DATA_PATH << "'" << endl;
+		return;
+	}
+
 	string codeHold;
 	double xHold, yHold;
 
@@ -40,14 +50,18 @@ service::service(std::string code) : m_serivceTypeCode(code)
 			m_locations.push_back(location(xHold, yHold));
 		}
 	}
-
 	file.close();
 
 	//grab name of service
 	file.open(SERVICES_CODES_PATH.c_str());
+	if (!file.is_open() || file.fail())
+	{
+		cerr << "ERROR (process " << rank << "): Could not open file '"
+			<< SERVICES_CODES_PATH << "'" << endl;
+		return;
+	}
 	string  lineHold , nameHold;
 	
-
 	while(file.good()) {
 		getline(file, codeHold, ',');
 		getline(file, nameHold);
--- Parrallel/ProximityAnalysis/ProximityAnalysis/main.cpp
+++ Parrallel/ProximityAnalysis/ProximityAnalysis/main.cpp
@@ -21,7 +21,7 @@ using namespace std;
 
 const string RESIDENCES_DATA_PATH = "data/residences.dat";
 
-const string USAGE = "ProximityAnalysis [serviceCode]";
+const string USAGE = "USAGE ERROR: Include the name of the input file on the command line!";
 
 vector<location> services;
 vector<double> distances;
@@ -29,12 +29,28 @@ vector<double> distances;
 double calcShortestStraightLineDistance(location, vector<location>);
 void proccessResidences();
 
-void proccessResidences(vector<location> const &services) {
-	ifstream file;
-	file.open(RESIDENCES_DATA_PATH.c_str());
+void proccessResidences(int rank, int numProcs,vector<location> const &services) {
+	ifstream file(RESIDENCES_DATA_PATH.c_str());
+	if (!file.is_open() || file.fail())
+	{
+		cerr << "ERROR (process " << rank << "): Could not open file '"
+			<< RESIDENCES_DATA_PATH << "'" << endl;
+		return;
+	}
+
+	string rec;
+	getline(file, rec);
+	int sinBytes = (int)file.tellg();
+
+	// Move the file pointer to MY first record
+	file.seekg(sinBytes * rank, ios::beg);
+
 	double xHold, yHold;
 	while(file >> xHold >> yHold) {
 		distances.push_back(calcShortestStraightLineDistance(location(xHold,yHold),services));
+
+		// Move file pointer to MY next record
+		file.seekg(sinBytes * (numProcs-1), ios::cur);
 	}
 }
 
@@ -62,8 +78,13 @@ double calcShortestStraightLineDistance(location r, vector<location> s) {
 
 int main (int argc, char* argv[]) 
 {
+	int numProcs, rank;
+	MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 	if(argc < 2) {
-		cout  << USAGE << endl;
+		if(rank = 0)
+			cerr  << USAGE << endl;
 		return EXIT_SUCCESS;
 	}
 
@@ -76,16 +97,21 @@ int main (int argc, char* argv[])
 	double start, end;
 	start = MPI_Wtime();
 
+	
+
+
+
 	service s(argv[1]);
-	proccessResidences(s.m_locations);
+	proccessResidences(rank,numProcs,s.m_locations);
 
 	end = MPI_Wtime();
 
-	s.print(cout);
-	cout.width(30); cout << left <<"Elapsed Time in Seconds: ";
-	cout.width(15); cout << right << end - start;
-	cout << endl << endl;
-
+	if(rank = 0) {
+		s.print(cout);
+		cout.width(30); cout << left <<"Elapsed Time in Seconds: ";
+		cout.width(15); cout << right << end - start;
+		cout << endl << endl;
+	}
 
 	distrabution dist(distances);
 	dist.print(cout);
